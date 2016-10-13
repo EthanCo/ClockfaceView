856bc85bec57d783af0a33381d6043cedb89d78c
EthanCo <fenghenyu@163.com>
2016-10-13 09:00:43 +0800

    init project

---
+++ clockface/src/main/java/com/ethanco/clockface/ClockfaceView.java
@@ -0,0 +1,24 @@
+package com.ethanco.clockface;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @Description 时间表盘 View
+ * Created by EthanCo on 2016/10/13.
+ */
+
+public class ClockfaceView extends View {
+    public ClockfaceView(Context context) {
+        super(context);
+    }
+
+    public ClockfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ClockfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
